0ea950752833e9256d96b6919bcd2d66a7272a30
Boshuai Li <liboshuai01@gmail.com>
2025-08-03 07:31:08 +0800

    refactor(flink-rpc):优化 ServerActor 日志输出
    - 移除了冗余的地址信息输出
    - 增加了回复客户端消息的日志记录
    - 简化了日志信息，使其更加清晰

--- flink-rpc/base-pekko-1/src/main/java/com/liboshuai/demo/server/ServerActor.java
+++ flink-rpc/base-pekko-1/src/main/java/com/liboshuai/demo/server/ServerActor.java
@@ -17,11 +17,12 @@ public class ServerActor extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .match(RequestData.class, requestData -> {
-                    log.info("接收到来自客户端的消息，内容为：[{}]，地址为: [{}]", requestData.getData(), getSender().path().toString());
+                    log.info("收到客户端的消息，内容为：[{}]", requestData.getData());
 
                     String replyMessage = String.format("消息 [%s] 已收到!", requestData.getData());
                     ResponseData response = new ResponseData(replyMessage);
 
+                    log.info("回应客户端的信息，内容为：[{}]", replyMessage);
                     getSender().tell(response, getSelf());
                 })
                 .matchAny(o -> log.warn("收到未知类型的消息: {}", o.getClass().getName()))
