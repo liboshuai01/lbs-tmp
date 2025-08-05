96693f680527e51617a75e40e950048f3dfb5743
Boshuai Li <liboshuai01@gmail.com>
2025-08-06 06:08:35 +0800

    test(BankTest): 优化线程等待逻辑以确保测试准确性
    - 移除了固定的 5秒等待时间，改为使用线程 join 方法确保两个用户线程都完成执行
    - 优化了输出信息，使其更准确地描述线程等待的目的

--- flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/safe/bank/BankTest.java
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/safe/bank/BankTest.java
@@ -1,7 +1,5 @@
 package com.liboshuai.demo.thread.safe.bank;
 
-import java.util.concurrent.TimeUnit;
-
 public class BankTest {
     public static void main(String[] args) throws InterruptedException {
         // 账号开始余额为0
@@ -10,10 +8,15 @@ public class BankTest {
         User user1 = new User(account, 10, 10000);
         // 用户二，每次存2元，存10000次
         User user2 = new User(account, 20, 10000);
-        new Thread(user1).start();
-        new Thread(user2).start();
-        System.out.println("主线程等待5秒，以便两个用户都完成存钱行为");
-        TimeUnit.SECONDS.sleep(5);
+        Thread thread1 = new Thread(user1);
+        Thread thread2 = new Thread(user2);
+        thread1.start();
+        thread2.start();
+
+        System.out.println("主线程等待两个用户都完成存钱行为");
+        thread1.join();
+        thread2.join();
+
         // 正确结果应该为300000，如果出现了线程安全问题，则会小于正确值
         System.out.printf("主线程最后获取到账户余额为: %d%n", account.getBalance());
     }
