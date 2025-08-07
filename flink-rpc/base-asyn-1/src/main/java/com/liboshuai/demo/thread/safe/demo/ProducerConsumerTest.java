dc8fa3f748c043e5943865dc60a71eefc8c6340b
Boshuai Li <liboshuai01@gmail.com>
2025-08-07 14:09:14 +0800

    feat(thread): 添加生产者消费者示例代码
    - 新增 ProducerConsumerTest 类，演示生产者和消费者线程的同步操作
    - 实现了 Factory 类，模拟产品生产和销售的过程
    - 添加了 Producer 和 Consumer 类，分别代表生产者和消费者线程
    - 通过 synchronized 关键字和 wait/notifyAll 方法实现了线程间的同步

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/safe/demo/ProducerConsumerTest.java
@@ -0,0 +1,138 @@
+package com.liboshuai.demo.thread.safe.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 案例2：生产者&消费者
+ * 生产者(Productor)将产品交给店员(Clerk)，而消费者(Customer)从店员处取走产品，店员一次只能持有
+ * 固定数量的产品(比如:20），如果生产者试图生产更多的产品，店员会叫生产者停一下，如果店中有空位放产品
+ * 了再通知生产者继续生产；如果店中没有产品了，店员会告诉消费者等一下，如果店中有产品了再通知消费者来
+ * 取走产品。
+ */
+public class ProducerConsumerTest {
+    public static void main(String[] args) throws InterruptedException {
+        Factory factory = new Factory(20);
+        Producer producer = new Producer(factory);
+        Consumer consumer = new Consumer(factory);
+        Thread t1 = new Thread(producer, "生产者");
+        Thread t2 = new Thread(consumer, "消费者");
+        t1.start();
+        t2.start();
+        System.out.println("主线程等待生产者消费者完成......");
+        t1.join();
+        t2.join();
+        System.out.println("主线程等待完毕......");
+    }
+}
+
+/**
+ * 工厂类提供生产、销售产品的能力，但是产品存能有限制
+ */
+class Factory {
+
+    /**
+     * 产品最大存量
+     */
+    private final int size;
+
+    /**
+     * 产品现有量
+     */
+    private final List<String> productList = new ArrayList<>();
+
+    public Factory(int size) {
+        this.size = size;
+    }
+
+    /**
+     * 制造产品的方法
+     */
+    public synchronized void madeProduct() {
+        if (productList.size() < size) {
+            productList.add("产品");
+            System.out.printf("线程 [%s] 生产了第一个产品，现存产品数量为: %d%n", Thread.currentThread().getName(), productList.size());
+            // 唤醒消费者线程
+            notifyAll();
+        } else {
+            // 如果现有的产品数量大于最大存储量，则暂停生产
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * 销售产品的方法
+     */
+    public synchronized void sellProduct() {
+        if (!productList.isEmpty()) {
+            System.out.printf("线程 [%s] 消费了第一个产品，现存产品数量为: %d%n", Thread.currentThread().getName(), productList.size());
+            productList.remove(0);
+            // 唤醒消费者线程
+            notifyAll();
+        } else {
+            // 如果现有的产品数量小于0，则暂停销售
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+/**
+ * 生产者线程，利用工厂一直不断生产产品
+ */
+class Producer implements Runnable{
+
+    private final Factory factory;
+
+    public Producer(Factory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            factory.madeProduct();
+        }
+    }
+}
+
+/**
+ * 消费者线程，从工厂源源不断购买产品
+ */
+class Consumer implements Runnable {
+
+    private final Factory factory;
+
+    public Consumer(Factory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            factory.sellProduct();
+        }
+    }
+
+}
