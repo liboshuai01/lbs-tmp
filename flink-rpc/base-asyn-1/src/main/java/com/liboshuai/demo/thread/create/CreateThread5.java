86f704f63e1364db6748cf7af6e07e9be0986220
Boshuai Li <liboshuai01@gmail.com>
2025-08-07 15:12:48 +0800

    feat(demo): 添加基于 CompletableFuture 的异步任务示例
    - 新增 CreateThread5 类，演示了如何使用 CompletableFuture 创建异步任务- 示例代码展示了任务提交、异步回调、异常处理等 CompletableFuture 的主要特性
    - 该示例有助于理解如何在 Java 中更高效地处理异步编程

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/create/CreateThread5.java
@@ -0,0 +1,44 @@
+package com.liboshuai.demo.thread.create;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CreateThread5 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            int sum = 0;
+            for (int i = 0; i < 100; i++) {
+                if (i % 2 == 0) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(i);
+                    sum += i;
+                }
+            }
+            return sum;
+        }, threadPool);
+        // 注册异步回调函数，当任务完成，自动调用这里的代码
+        completableFuture.thenAccept(result -> System.out.println("最终计算结果为:" + result));
+        // 注册异常处理回调函数，当任务异常，自动调用这里的代码
+        completableFuture.exceptionally(throwable -> {
+            System.out.println("计算异常：" + throwable.getMessage());
+            return null; // 返回默认值null
+        });
+        // 主线程可以继续做其他事情，完全不需要关系异步任务什么时候结束
+        System.out.println("主线程：任务已提交，继续做自己的事情......");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("主线程正在执行其他任务... " + i);
+            TimeUnit.MILLISECONDS.sleep(300);
+        }
+        // 异步任务结束后，关闭线程池
+        completableFuture.join();
+        threadPool.shutdown();
+        System.out.println("主线程：任务执行完毕，线程池已关闭......");
+    }
+}
