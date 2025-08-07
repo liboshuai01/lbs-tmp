82b492721a6c147114be3b511b92d804198e773b
Boshuai Li <liboshuai01@gmail.com>
2025-08-08 02:36:06 +0800

    feat(thread): 添加 CalcTest程序用于演示线程安全的 wait 示例
    - 新增 CalcTest 类作为程序入口
    - 创建 Calc 类实现加法和减法的同步方法
    - 设计 CalcTask 类以循环执行加法或减法操作
    - 通过线程通信（wait 和 notifyAll）实现线程安全的并发控制

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/safe/wait/CalcTest.java
@@ -0,0 +1,78 @@
+package com.liboshuai.demo.thread.safe.wait;
+
+import java.util.concurrent.TimeUnit;
+
+public class CalcTest {
+    public static void main(String[] args) throws InterruptedException {
+        Calc calc = new Calc();
+        Thread t1 = new Thread(new CalcTask(true, calc), "加法线程");
+        Thread t2 = new Thread(new CalcTask(false, calc), "减法线程");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
+
+class Calc {
+    private int m = 0;
+
+    public synchronized void add() {
+        if (m == 0) {
+            m++;
+            System.out.printf("[%s] 执行了一次加一，现 m 值为: [%d]%n", Thread.currentThread().getName(), m);
+            notifyAll();
+        } else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void sub() {
+        if (m == 1) {
+            m--;
+            System.out.printf("[%s] 执行了一次减一，现 m 值为: [%d]%n", Thread.currentThread().getName(), m);
+            notifyAll();
+        } else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class CalcTask implements Runnable {
+
+    private final boolean isAdd;
+    private final Calc calc;
+
+    public CalcTask(boolean isAdd, Calc calc) {
+        this.isAdd = isAdd;
+        this.calc = calc;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (isAdd) {
+                calc.add();
+            } else {
+                calc.sub();
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
