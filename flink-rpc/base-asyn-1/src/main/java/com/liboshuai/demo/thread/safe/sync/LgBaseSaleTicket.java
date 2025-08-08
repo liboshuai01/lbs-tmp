d8b532ee875c7470d26e06ff96c6192f71c44ba6
Boshuai Li <liboshuai01@gmail.com>
2025-08-09 04:26:20 +0800

    feat(thread): 添加基于 CAS 的线程安全卖票示例
    - 新增 LgBaseSaleTicket 类，包含主程序和相关类
    - 使用 AtomicInteger 实现线程安全的票数计数
    - 通过 compareAndSet 方法确保并发操作的安全性
    - 模拟多线程环境下售票过程，展示 CAS 的应用

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/thread/safe/sync/LgBaseSaleTicket.java
@@ -0,0 +1,59 @@
+package com.liboshuai.demo.thread.safe.sync;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LgBaseSaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket(30);
+        SaleTask task = new SaleTask(ticket);
+        new Thread(task, "线程1").start();
+        new Thread(task, "线程2").start();
+        new Thread(task, "线程3").start();
+    }
+
+    static class Ticket {
+        private final AtomicInteger ticketCount;
+
+        public Ticket(int ticketCount) {
+            this.ticketCount = new AtomicInteger(ticketCount);
+        }
+
+        public boolean sale() {
+            boolean result = false;
+            while (true) {
+                int currentCount = ticketCount.get();
+                if (currentCount > 0) {
+                    if (ticketCount.compareAndSet(currentCount, currentCount - 1)) {
+                        System.out.println(Thread.currentThread().getName() + " 卖了第 " + currentCount + " 张票");
+                        result = true;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+
+    static class SaleTask implements Runnable {
+
+        private final Ticket ticket;
+
+        public SaleTask(Ticket ticket) {
+            this.ticket = ticket;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                boolean result = ticket.sale();
+                if (!result) {
+                    System.out.println(Thread.currentThread().getName() + " 票已经售尽，结束买票！");
+                    break;
+                }
+            }
+        }
+
+    }
+}
