8c9b0482c072ab62227e2a70cb3245f389760246
Boshuai Li <liboshuai01@gmail.com>
2025-08-13 07:26:52 +0800

    demo(threadlocal): 添加 ThreadLocal内存泄漏示例代码
    - 新增 ThreadLocalMemoryLeakExample 类，演示 ThreadLocal 可能导致的内存泄漏问题
    - 使用单线程执行器和 ThreadLocal<byte[]> 进行测试
    - 循环执行任务，每次任务中设置新的 byte[] 到 ThreadLocal
    - 展示了旧值未被释放，可能导致内存泄漏的情况

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/threadlocal/ThreadLocalMemoryLeakExample.java
@@ -0,0 +1,24 @@
+package com.liboshuai.demo.threadlocal;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadLocalMemoryLeakExample {
+    public static void main(String[] args) throws InterruptedException {
+        final ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        final ThreadLocal<byte[]> threadLocal = new ThreadLocal<>();
+        for (int i = 0; i < 3; i++) {
+            threadPool.execute(() -> {
+                String threadName = Thread.currentThread().getName();
+                byte[] old_bytes = threadLocal.get();
+                System.out.printf("[%s] 获取到旧值 [%s]%n", threadName, old_bytes);
+                byte[] new_bytes = new byte[1024 * 1024];
+                threadLocal.set(new_bytes);
+                System.out.printf("[%s] 设置了新值 [%s]%n", threadName, old_bytes);
+            });
+            TimeUnit.SECONDS.sleep(1);
+        }
+        threadPool.shutdown();
+    }
+}
