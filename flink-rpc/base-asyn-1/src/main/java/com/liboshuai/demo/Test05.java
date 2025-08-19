e47e2142eeb381830b05b3517d90e6900e3871e7
Boshuai Li <liboshuai01@gmail.com>
2025-08-19 13:39:49 +0800

    test(demo): 添加 Test05 类进行并发测试
    新增 Test05 类，用于测试在并发情况下整数增减操作的结果。该测试创建两个线程，分别执行 1000 次递增和递减操作，最后输出最终结果。

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/Test05.java
@@ -0,0 +1,41 @@
+package com.liboshuai.demo;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test05 {
+
+    private static int num = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                num++;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                num--;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("最终num的值为: " + num);
+    }
+
+
+}
