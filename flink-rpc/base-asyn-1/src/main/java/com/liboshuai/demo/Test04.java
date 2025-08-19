526a10ff154cafc8c2e0d717c4a7915794a0a83c
Boshuai Li <liboshuai01@gmail.com>
2025-08-19 13:03:24 +0800

    feat(demo): 添加 Test04 类演示协同程序
    - 新增 Test04 类，模拟泡茶过程中的异步任务执行
    - 包含两个任务类 TaskA 和 TaskB，分别代表不同的泡茶步骤
    - 使用多线程实现任务并行执行，并通过 join 方法确保任务顺序
    - 输出任务执行时间和结果，展示异步编程的优势

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/Test04.java
@@ -0,0 +1,70 @@
+package com.liboshuai.demo;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+public class Test04 {
+    public static void main(String[] args) throws InterruptedException {
+        Instant start = Instant.now();
+        Thread t1 = new Thread(new TaskA());
+        Thread t2 = new Thread(new TaskB(t1));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        Instant end = Instant.now();
+        System.out.println("耗时：" + Duration.between(start, end).toMillis()); // 预期7秒多
+    }
+
+    /**
+     * 1. 洗水壶
+     * 2. 烧开水
+     */
+    static class TaskA implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("洗水壶完成");
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("烧开水完成");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * 1. 洗茶壶
+     * 2. 洗茶杯
+     * 3. 拿茶叶
+     */
+    static class TaskB implements Runnable{
+
+        private final Thread thread;
+
+        TaskB(Thread thread) {
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("洗茶壶完成");
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("洗茶杯完成");
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("拿茶叶完成");
+                System.out.println("等待TaskA烧开水完成");
+                thread.join();
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println("泡茶完成");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
