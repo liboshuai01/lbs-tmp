ac9fe9b426cd508de2152c14525c44b4a462e31c
Boshuai Li <liboshuai01@gmail.com>
2025-08-20 13:47:29 +0800

    feat(demo): 添加 Test11 类用于演示守护对象模式
    新增 Test11 类，实现了守护对象模式用于线程间通信。该示例展示了如何通过共享对象实现线程间的同步和通信，以解决生产者-消费者问题。

---
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/Test11.java
@@ -0,0 +1,57 @@
+package com.liboshuai.demo;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test11 {
+    public static void main(String[] args) throws InterruptedException {
+        GuardedObject guardedObject = new GuardedObject();
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "开始获取响应值...");
+            Object response = guardedObject.get();
+            System.out.println(Thread.currentThread().getName() + "得到了响应值: " + response);
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            String response = "我就是结果，服不服！";
+            guardedObject.completed(response);
+        }, "t2");
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+    static class GuardedObject {
+        private Object response;
+
+        public Object get() {
+            synchronized (this) {
+                while (response == null) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+                return response;
+            }
+        }
+
+        public void completed(Object response) {
+            synchronized (this) {
+                this.response = response;
+                this.notifyAll();
+            }
+        }
+    }
+}
