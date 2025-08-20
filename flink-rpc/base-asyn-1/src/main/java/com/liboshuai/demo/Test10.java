e5484975d5261dcc4508e067d9eb27979dafd8ae
Boshuai Li <liboshuai01@gmail.com>
2025-08-20 10:02:01 +0800

    refactor(demo): 重构 Test10 类中的 Account 类
    - 将 Account 类从独立类改为 Test10 类的静态内部类
    - 优化了 transfer 方法的同步块，提高了线程安全性

--- flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/Test10.java
+++ flink-rpc/base-asyn-1/src/main/java/com/liboshuai/demo/Test10.java
@@ -31,28 +31,28 @@ public class Test10 {
     public static int randomAmount() {
         return random.nextInt(100) + 1;
     }
-}
 
-class Account {
-    private int money;
+    static class Account {
+        private int money;
 
-    public Account(int money) {
-        this.money = money;
-    }
+        public Account(int money) {
+            this.money = money;
+        }
 
-    public int getMoney() {
-        return money;
-    }
+        public int getMoney() {
+            return money;
+        }
 
-    public void setMoney(int money) {
-        this.money = money;
-    }
+        public void setMoney(int money) {
+            this.money = money;
+        }
 
-    public void transfer(Account target, int amount) {
-        synchronized (Account.class) {
-            if (this.money > amount) {
-                this.setMoney(this.getMoney() - amount);
-                target.setMoney(target.getMoney() + amount);
+        public void transfer(Account target, int amount) {
+            synchronized (Account.class) {
+                if (this.money > amount) {
+                    this.setMoney(this.getMoney() - amount);
+                    target.setMoney(target.getMoney() + amount);
+                }
             }
         }
     }
