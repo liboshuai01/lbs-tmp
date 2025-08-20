3cdd9bd1834f2c916033fbfc52f49ebd47335723
Boshuai Li <liboshuai01@gmail.com>
2025-08-20 20:22:07 +0800

    demo: 添加 Test17 类演示线程阻塞和唤醒
    新增 Test17 类，包含 main 方法用于演示线程阻塞和唤醒的功能。程序创建两个线程 t1 和 t2，t1线程调用 LockSupport.park() 方法进行阻塞，t2 线程调用 LockSupport.unpark(t1) 方法唤醒 t1 线程。

---
+++ flink-rpc/base-juc/src/main/java/com/liboshuai/demo/Test17.java
@@ -0,0 +1,21 @@
+package com.liboshuai.demo;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class Test17 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            LockSupport.park();
+            System.out.println("A...");
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            LockSupport.unpark(t1);
+            System.out.println("B...");
+        }, "t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
