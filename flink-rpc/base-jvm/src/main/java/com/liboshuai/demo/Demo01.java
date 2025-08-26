a4ac761fc027ac53c86e77438399aec62f194380
Boshuai Li <liboshuai01@gmail.com>
2025-08-27 04:47:26 +0800

    update Demo01.java

--- flink-rpc/base-jvm/src/main/java/com/liboshuai/demo/Demo01.java
+++ flink-rpc/base-jvm/src/main/java/com/liboshuai/demo/Demo01.java
@@ -16,5 +16,9 @@ public class Demo01 {
         // 获取其上层：
         ClassLoader bootstrapClassLoader = extClassLoader.getParent();
         log.info("bootstrapClassLoader: {}", bootstrapClassLoader);
+
+        // 对于用户自定义类来说：
+        ClassLoader classLoader = Demo01.class.getClassLoader();
+        log.info("classLoader: {}", classLoader);
     }
 }
