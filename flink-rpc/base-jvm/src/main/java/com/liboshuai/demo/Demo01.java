99105fccc2e1519134f65a22d21fb0e18757f136
Boshuai Li <liboshuai01@gmail.com>
2025-08-27 04:57:42 +0800

    update Demo01.java

--- flink-rpc/base-jvm/src/main/java/com/liboshuai/demo/Demo01.java
+++ flink-rpc/base-jvm/src/main/java/com/liboshuai/demo/Demo01.java
@@ -13,12 +13,16 @@ public class Demo01 {
         ClassLoader extClassLoader = systemClassLoader.getParent();
         log.info("extClassLoader: {}", extClassLoader);
 
-        // 获取其上层：
+        // 获取其上层：获取不到引导类加载器
         ClassLoader bootstrapClassLoader = extClassLoader.getParent();
         log.info("bootstrapClassLoader: {}", bootstrapClassLoader);
 
-        // 对于用户自定义类来说：
-        ClassLoader classLoader = Demo01.class.getClassLoader();
-        log.info("classLoader: {}", classLoader);
+        // 对于用户自定义类来说：默认使用系统类加载器进行加载
+        ClassLoader demo01ClassLoader = Demo01.class.getClassLoader();
+        log.info("demo01ClassLoader: {}", demo01ClassLoader);
+
+        // String类使用引导类加载器进行加载（Java的核心类库都是使用引导类加载器进行加载的）
+        ClassLoader stringClassLoader = String.class.getClassLoader();
+        log.info("stringClassLoader: {}", stringClassLoader);
     }
 }
