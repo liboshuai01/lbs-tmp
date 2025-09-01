3f649fdd10ea6ee2522c627dc3b54c70d557528f
Boshuai Li <liboshuai01@gmail.com>
2025-09-02 02:12:23 +0800

    add demo01

--- netty-heima/src/main/java/com/liboshuai/demo/Demo01.java
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo01.java
@@ -5,22 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class Demo01 {
     public static void main(String[] args) {
         try (FileChannel channel = new FileInputStream("netty-heima/data/demo01.txt").getChannel()) {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
-            Charset charset = StandardCharsets.UTF_8;
-            while (channel.read(byteBuffer) != -1) {
-                byteBuffer.flip();
-                CharBuffer charBuffer = charset.decode(byteBuffer);
-                log.info("{}", charBuffer);
-                byteBuffer.clear();
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true) {
+                int length = channel.read(buffer);
+                if (length == -1) {
+                    break;
+                }
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    byte data = buffer.get();
+                    log.info("{}", (char) data);
+                }
+                buffer.clear();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
