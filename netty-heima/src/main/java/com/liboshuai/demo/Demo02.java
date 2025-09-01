f9d9e8fe2e8b88aaf593e37125656b8272437368
Boshuai Li <liboshuai01@gmail.com>
2025-09-02 02:16:51 +0800

    add Demo02

---
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo02.java
@@ -0,0 +1,29 @@
+package com.liboshuai.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class Demo02 {
+    public static void main(String[] args) {
+        try (FileChannel channel = new FileInputStream("netty-heima/data/demo02.txt").getChannel()) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+            Charset charset = StandardCharsets.UTF_8;
+            while (channel.read(byteBuffer) != -1) {
+                byteBuffer.flip();
+                CharBuffer charBuffer = charset.decode(byteBuffer);
+                log.info("{}", charBuffer);
+                byteBuffer.clear();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
