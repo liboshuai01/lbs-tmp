7d758badaa22a40f910b566c44b5e0d02cdc7e28
Boshuai Li <liboshuai01@gmail.com>
2025-09-02 06:13:14 +0800

    add Demo09

---
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo09.java
@@ -0,0 +1,25 @@
+package com.liboshuai.demo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        ByteBuffer buffer1 = StandardCharsets.UTF_8.encode("hello");
+        ByteBuffer buffer2 = StandardCharsets.UTF_8.encode("World");
+        ByteBuffer buffer3 = StandardCharsets.UTF_8.encode("你好");
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile("netty-heima/data/demo09.txt", "rw")) {
+            FileChannel fileChannel = randomAccessFile.getChannel();
+            long result = fileChannel.write(new ByteBuffer[]{buffer1, buffer2, buffer3});
+            System.out.println("result: " + result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
