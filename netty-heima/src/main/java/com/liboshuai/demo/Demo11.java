f00de5208490788256dd1138f5553e81c5373332
Boshuai Li <liboshuai01@gmail.com>
2025-09-02 07:29:44 +0800

    add Demo11

---
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo11.java
@@ -0,0 +1,16 @@
+package com.liboshuai.demo;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+public class Demo11 {
+    public static void main(String[] args) {
+        try (FileChannel sourceFileChannel = new RandomAccessFile("netty-heima/data/demo10_source.txt", "r").getChannel();
+             FileChannel targetFileChannel = new RandomAccessFile("netty-heima/data/demo10_target.txt", "rw").getChannel()) {
+            sourceFileChannel.transferTo(0, sourceFileChannel.size(), targetFileChannel);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
