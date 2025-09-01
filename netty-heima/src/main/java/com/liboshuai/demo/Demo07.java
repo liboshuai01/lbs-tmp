d76cbd9a1c6c284be0025e1a708225d2d2480df4
Boshuai Li <liboshuai01@gmail.com>
2025-09-02 05:44:47 +0800

    add Demo07

--- netty-heima/src/main/java/com/liboshuai/demo/Demo07.java
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo07.java
@@ -12,13 +12,20 @@ public class Demo07 {
         ByteBuffer buffer1 = ByteBuffer.allocate(16);
         buffer1.put("hello".getBytes());
         ByteBufferUtil.debugAll("方法一", buffer1);
+        buffer1.flip();
+        String string1 = StandardCharsets.UTF_8.decode(buffer1).toString();
+        System.out.println("string1: " + string1);
 
         // 2. 方法二
         ByteBuffer buffer2 = StandardCharsets.UTF_8.encode("hello");
         ByteBufferUtil.debugAll("方法二", buffer2);
+        String string2 = StandardCharsets.UTF_8.decode(buffer2).toString();
+        System.out.println("string2: " + string2);
 
         // 3. 方法三
         ByteBuffer buffer3 = ByteBuffer.wrap("hello".getBytes());
         ByteBufferUtil.debugAll("方法三", buffer3);
+        String string3 = StandardCharsets.UTF_8.decode(buffer3).toString();
+        System.out.println("string3: " + string3);
     }
 }
