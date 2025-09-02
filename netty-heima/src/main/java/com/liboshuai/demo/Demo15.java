ebecb430156b30ffe3b3be0991d0bd077b704dd4
Boshuai Li <liboshuai01@gmail.com>
2025-09-03 04:31:47 +0800

    add Demo15

--- netty-heima/src/main/java/com/liboshuai/demo/Demo15.java
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo15.java
@@ -3,6 +3,7 @@ package com.liboshuai.demo;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
@@ -26,14 +27,27 @@ public class Demo15 {
                         if (key.isAcceptable()) {
                             ServerSocketChannel sscChannel = (ServerSocketChannel) key.channel();
                             SocketChannel sc = sscChannel.accept();
+                            sc.configureBlocking(false);
+                            SelectionKey scKey = sc.register(selector, SelectionKey.OP_READ);
                             StringBuilder sb = new StringBuilder();
                             for (int i = 0; i < 300000000; i++) {
                                 sb.append("a");
                             }
                             ByteBuffer buffer = StandardCharsets.UTF_8.encode(sb.toString());
-                            while (buffer.hasRemaining()) {
-                                int length = sc.write(buffer);
-                                System.out.println("length: " + length);
+                            int length = sc.write(buffer);
+                            System.out.println("length1: " + length);
+                            if (buffer.hasRemaining()) {
+                                scKey.interestOps(scKey.interestOps() + SelectionKey.OP_WRITE);
+                                scKey.attach(buffer);
+                            }
+                        } else if (key.isWritable()) {
+                            ByteBuffer buffer = (ByteBuffer) key.attachment();
+                            SocketChannel sc = (SocketChannel) key.channel();
+                            int length = sc.write(buffer);
+                            System.out.println("length2: " + length);
+                            if (!buffer.hasRemaining()) {
+                                key.attach(null);
+                                key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);
                             }
                         }
                     }
