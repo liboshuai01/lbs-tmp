952564e2e42819466884794703dc39b62c3d9d4f
Boshuai Li <liboshuai01@gmail.com>
2025-09-03 01:20:08 +0800

    add Demo13

--- netty-heima/src/main/java/com/liboshuai/demo/Demo13.java
+++ netty-heima/src/main/java/com/liboshuai/demo/Demo13.java
@@ -9,31 +9,29 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Demo12 {
-
+public class Demo13 {
     static class Server {
         public static void main(String[] args) {
             try {
-                ByteBuffer buffer = ByteBuffer.allocate(256);
-                // 1. 创建服务器
                 ServerSocketChannel ssc = ServerSocketChannel.open();
-                // 2. 绑定端口
                 ssc.bind(new InetSocketAddress(8080));
-                // 3. 创建连接集合
+                ssc.configureBlocking(false); // 设置 ServerSocketChannel 为非阻塞，影响 ssc.accept()
+                ByteBuffer buffer = ByteBuffer.allocate(256);
                 List<SocketChannel> scList = new ArrayList<>();
                 while (true) {
-                    // 4. 等待建立连接，以便获取与客户端通信的 SocketChannel
-                    System.out.println("等待连接建立前......");
-                    SocketChannel sc = ssc.accept(); // 默认为阻塞
-                    System.out.println("连接成功被建立......");
-                    scList.add(sc);
+                    SocketChannel sc = ssc.accept();
+                    if (sc != null) {
+                        System.out.println("连接建立成功......");
+                        sc.configureBlocking(false); // 设置 SocketChannel 为非阻塞，影响 sc.read()
+                        scList.add(sc);
+                    }
                     for (SocketChannel socketChannel : scList) {
-                        // 5. 接收到客户端发送的数据
-                        System.out.println("等待接收数据中......");
-                        socketChannel.read(buffer); // 默认为阻塞
-                        buffer.flip();
-                        System.out.println("接收到来自客户端的信息："+ StandardCharsets.UTF_8.decode(buffer));
-                        buffer.clear();
+                        int length = socketChannel.read(buffer);
+                        if (length > 0) {
+                            buffer.flip();
+                            System.out.println("接收到来自客户端的信息：" + StandardCharsets.UTF_8.decode(buffer));
+                            buffer.clear();
+                        }
                     }
                 }
             } catch (IOException e) {
@@ -42,13 +40,13 @@ public class Demo12 {
         }
     }
 
-    static class client {
+    static class Client {
         public static void main(String[] args) {
             try {
                 SocketChannel sc = SocketChannel.open();
                 sc.connect(new InetSocketAddress("127.0.0.1", 8080));
-                // 打断点
-                System.out.println("已经与服务端建立了连接......");
+                // 这里打断点（手动执行`sc.write(StandardCharsets.UTF_8.encode("333"))`）
+                System.out.println("连接建立成功......");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
