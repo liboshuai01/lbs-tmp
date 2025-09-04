a885b64e1de7717ce348a8f180eebdda2847792b
Boshuai Li <liboshuai01@gmail.com>
2025-09-05 07:57:29 +0800

    add Demo24

---
+++ flink-src-learn/base-netty/src/main/java/com/liboshuai/demo/Demo24.java
@@ -0,0 +1,36 @@
+package com.liboshuai.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+import static io.netty.buffer.ByteBufUtil.appendPrettyHexDump;
+import static io.netty.util.internal.StringUtil.NEWLINE;
+
+@Slf4j
+public class Demo24 {
+    public static void main(String[] args) {
+        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+        log(byteBuf);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 300; i++) {
+            sb.append("a");
+        }
+        byteBuf.writeBytes(sb.toString().getBytes(StandardCharsets.UTF_8));
+        log(byteBuf);
+    }
+
+    public static void log(ByteBuf buffer) {
+        int length = buffer.readableBytes();
+        int rows = length / 16 + (length % 15 == 0 ? 0 : 1) + 4;
+        StringBuilder buf = new StringBuilder(rows * 80 * 2)
+                .append("read index:").append(buffer.readerIndex())
+                .append(" write index:").append(buffer.writerIndex())
+                .append(" capacity:").append(buffer.capacity())
+                .append(NEWLINE);
+        appendPrettyHexDump(buf, buffer);
+        log.info("{}", buf);
+    }
+}
