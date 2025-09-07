c5769bdf11f41e4d28912f0dade3251108c75f7a
Boshuai Li <liboshuai01@gmail.com>
2025-09-07 10:54:35 +0800

    add MessageCodecTest

--- flink-src-learn/base-netty/src/test/java/com/liboshuai/demo/chat/protocol/MessageCodecTest.java
+++ flink-src-learn/base-netty/src/test/java/com/liboshuai/demo/chat/protocol/MessageCodecTest.java
@@ -4,6 +4,7 @@ import com.liboshuai.demo.message.LoginRequestMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.logging.LoggingHandler;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,7 @@ class MessageCodecTest {
     @Test
     void testEncodeAndDecode() throws Exception {
         EmbeddedChannel embeddedChannel = new EmbeddedChannel(
+                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
                 new LoggingHandler(),
                 new MessageCodec()
         );
@@ -23,10 +25,17 @@ class MessageCodecTest {
         LoginRequestMessage message = new LoginRequestMessage("lbs", "YOUR_PASSWORD");
         embeddedChannel.writeOutbound(message);
 
+        System.out.println("=============================================================");
+
         // decode
         ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
         new MessageCodec().encode(null, message, buf);
-        embeddedChannel.writeInbound(buf);
+        ByteBuf buf1 = buf.slice(0, 100);
+        buf1.retain();
+        ByteBuf buf2 = buf.slice(100, buf.readableBytes() - 100);
+        buf2.retain();
+        embeddedChannel.writeInbound(buf1);
+        embeddedChannel.writeInbound(buf2);
     }
 }
 
