f31be50e497594a748e52b279031ad977432a93c
Boshuai Li <liboshuai01@gmail.com>
2025-09-07 11:50:26 +0800

    add MessageCodec

--- flink-src-learn/base-netty/src/main/java/com/liboshuai/demo/chat/protocol/MessageCodec.java
+++ flink-src-learn/base-netty/src/main/java/com/liboshuai/demo/chat/protocol/MessageCodec.java
@@ -2,8 +2,9 @@ package com.liboshuai.demo.chat.protocol;
 
 import com.liboshuai.demo.message.Message;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageCodec;
+import io.netty.handler.codec.MessageToMessageCodec;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayInputStream;
@@ -13,59 +14,60 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 @Slf4j
-public class MessageCodec extends ByteToMessageCodec<Message> {
-    /**
-     * 编码：将消息转为ByteBuf
-     */
+@ChannelHandler.Sharable
+public class MessageCodec extends MessageToMessageCodec<ByteBuf, Message> {
+
+    public static final MessageCodec INSTANCE = new MessageCodec();
+
+
     @Override
-    protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
+    protected void encode(ChannelHandlerContext ctx, Message msg, List<Object> out) throws Exception {
+        ByteBuf buffer = ctx.alloc().buffer();
         // 1. 4字节的魔数
-        out.writeBytes(new byte[]{1,2,3,4});
+        buffer.writeBytes(new byte[]{1,2,3,4});
         // 2. 1字节的版本
-        out.writeByte(1);
+        buffer.writeByte(1);
         // 3. 1字节的序列化方式（jdk-0，json-1）
-        out.writeByte(0);
+        buffer.writeByte(0);
         // 4. 1字节的指令类型
-        out.writeByte(msg.getMessageType());
+        buffer.writeByte(msg.getMessageType());
         // 5. 4字节的消息序号（int）
-        out.writeInt(msg.getSequenceId());
+        buffer.writeInt(msg.getSequenceId());
         // 6. 1字节为了对其2的倍数填充字节
-        out.writeByte(0xff);
+        buffer.writeByte(0xff);
         // 获取内容的字节数组
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(bos);
         oos.writeObject(msg);
         byte[] bytes = bos.toByteArray();
         // 7. 4字节的信息长度（int）
-        out.writeInt(bytes.length);
+        buffer.writeInt(bytes.length);
         // 8. 剩下都为消息内容
-        out.writeBytes(bytes);
+        buffer.writeBytes(bytes);
+        out.add(buffer);
     }
 
-    /**
-     * 解码：将ByteBuf解析为消息
-     */
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
         // 1. 4字节的魔数
         byte[] magicBytes = new byte[4];
-        in.readBytes(magicBytes);
+        msg.readBytes(magicBytes);
         // 2. 1字节的版本
-        byte version = in.readByte();
+        byte version = msg.readByte();
         // 3. 1字节的序列化方式（jdk-0，json-1）
-        byte serializerType = in.readByte();
+        byte serializerType = msg.readByte();
         // 4. 1字节的指令类型
-        byte messageType = in.readByte();
+        byte messageType = msg.readByte();
         // 5. 4字节的消息序号（int）
-        int sequenceId = in.readInt();
+        int sequenceId = msg.readInt();
         // 6. 1字节为了对其2的倍数填充字节
-        in.readByte();
+        msg.readByte();
         // 获取内容的字节数组
         // 7. 4字节的信息长度（int）
-        int length = in.readInt();
+        int length = msg.readInt();
         // 8. 剩下都为消息内容
         byte[] messageBytes = new byte[length];
-        in.readBytes(messageBytes, 0, length);
+        msg.readBytes(messageBytes, 0, length);
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(messageBytes));
         Message message = (Message) ois.readObject();
         out.add(message);
--- flink-src-learn/base-netty/src/main/java/com/liboshuai/demo/chat/server/ChatServer.java
+++ flink-src-learn/base-netty/src/main/java/com/liboshuai/demo/chat/server/ChatServer.java
@@ -1,8 +1,54 @@
 package com.liboshuai.demo.chat.server;
 
+import com.liboshuai.demo.chat.protocol.MessageCodec;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class ChatServer {
+
+    private static final int PORT = 8080;
+
+    private static final LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+
     public static void main(String[] args) {
+
+
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup(2);
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0));
+                            pipeline.addLast(LOGGING_HANDLER);
+                            pipeline.addLast(MessageCodec.INSTANCE);
+                        }
+                    });
+            Channel channel = serverBootstrap.bind(PORT).sync().channel();
+            log.info("服务器启动成功，端口: [{}]", PORT);
+            channel.closeFuture().sync(); // 阻塞等待关闭服务器
+        } catch (InterruptedException e) {
+            log.error("服务器被中断", e);
+            Thread.currentThread().interrupt();
+        } finally {
+            log.info("服务器正在关闭...");
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
     }
 }
