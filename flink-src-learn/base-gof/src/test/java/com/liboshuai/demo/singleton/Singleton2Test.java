a3727e6a6cd9e3aa36b1038dc19fd701cb5d4a36
Boshuai Li <liboshuai01@gmail.com>
2025-09-08 09:35:12 +0800

    add testSerializer

--- flink-src-learn/base-gof/src/test/java/com/liboshuai/demo/singleton/Singleton2Test.java
+++ flink-src-learn/base-gof/src/test/java/com/liboshuai/demo/singleton/Singleton2Test.java
@@ -1,12 +1,21 @@
 package com.liboshuai.demo.singleton;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 饿汉式: 静态代码块形式
  */
+@Slf4j
 class Singleton2Test {
 
     @Test
@@ -18,6 +27,38 @@ class Singleton2Test {
         assertNotNull(instance2, "单例实例不应该为 null");
 
         assertSame(instance1, instance2, "多次获取的实例应该是同一个对象");
+    }
+
+    @Test
+    void testSerializer() {
+        Path path = Paths.get("C:/Users/lbs/me/project/flink-project/lbs-tmp/flink-src-learn/base-gof/data/singleton.txt");
+
+        // 序列化之前，先获取原始的单例实例
+        Singleton2 originalInstance = Singleton2.getInstance();
+
+        try {
+            // 步骤 1: 将对象序列化到文件
+            try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+                oos.writeObject(originalInstance);
+            }
+
+            // 步骤 2: 从文件中反序列化对象
+            Singleton2 deserializedInstance;
+            try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+                deserializedInstance = (Singleton2) ois.readObject();
+            }
+
+            // 步骤 3: 断言结果
+            log.info("原始实例的哈希值: {}", originalInstance.hashCode());
+            log.info("反序列化实例的哈希值: {}", deserializedInstance.hashCode());
+
+            assertSame(originalInstance, deserializedInstance, "反序列化后应该返回同一个实例");
+
+        } catch (IOException | ClassNotFoundException e) {
+            log.error("序列化或反序列化Java对象出现异常", e);
+            // 如果发生异常，显式地让测试失败
+            fail("测试因异常而失败: " + e.getMessage());
+        }
     }
 
 }
