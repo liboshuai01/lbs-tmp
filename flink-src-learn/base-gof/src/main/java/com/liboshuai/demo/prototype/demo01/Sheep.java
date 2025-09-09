b38a34c5e6e00c45ccc0b72d48ebdbaf41ef4ddd
Boshuai Li <liboshuai01@gmail.com>
2025-09-09 09:34:14 +0800

    add Sheep

--- flink-src-learn/base-gof/src/main/java/com/liboshuai/demo/prototype/demo01/Sheep.java
+++ flink-src-learn/base-gof/src/main/java/com/liboshuai/demo/prototype/demo01/Sheep.java
@@ -3,11 +3,9 @@ package com.liboshuai.demo.prototype.demo01;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 
-@Slf4j
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
