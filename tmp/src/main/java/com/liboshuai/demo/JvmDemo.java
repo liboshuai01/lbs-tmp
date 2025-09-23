1c6a8ec3ef3e81ebbc6c07cf5fdf3f5f684be265
Boshuai Li <liboshuai01@gmail.com>
2025-09-24 04:53:49 +0800

    update JvmDemo

--- tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
+++ tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
@@ -2,25 +2,10 @@ package com.liboshuai.demo;
 
 public class JvmDemo {
     public static void main(String[] args) {
-        String s1= "javaEE";
-        String s2 = "hadoop";
-
-        String s3 = "javaEEhadoop";
-        String s4 = "javaEE" + "hadoop";
-        String s5 = s1 + "hadoop";
-        String s6 = "javaEE" + s2;
-        String s7 = s1 + s2;
-
-        System.out.println(s3 == s4); // true
-        System.out.println(s3 == s5); // false
-        System.out.println(s3 == s6); // false
-        System.out.println(s3 == s7); // false
-        System.out.println(s5 == s6); // false
-        System.out.println(s5 == s7); // false
-        System.out.println(s6 == s7); // false
-
-        String s8 = s6.intern();
-        System.out.println(s3 == s6); // false
-        System.out.println(s3 == s8); // true
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "ab";
+        String s4 = s1 + s2;
+        System.out.println(s3 == s4); // false
     }
 }
