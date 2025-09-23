9f334f041511c09b4ea156dbe4c973d502a54094
Boshuai Li <liboshuai01@gmail.com>
2025-09-24 05:02:56 +0800

    update JvmDemo

--- tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
+++ tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
@@ -2,10 +2,10 @@ package com.liboshuai.demo;
 
 public class JvmDemo {
     public static void main(String[] args) {
-        String s1 = "a";
-        String s2 = "b";
-        String s3 = "ab";
+        final String s1 = "a";
+        final String s2 = "b";
+        String s3 = "a" + "b";
         String s4 = s1 + s2;
-        System.out.println(s3 == s4); // false
+        System.out.println(s3 == s4); // true
     }
 }
