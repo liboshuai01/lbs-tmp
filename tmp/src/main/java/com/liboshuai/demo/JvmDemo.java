f96371b2b0bae4fdbba7869ef634beaa16543465
Boshuai Li <liboshuai01@gmail.com>
2025-09-24 20:07:43 +0800

    update JvmDemo

--- tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
+++ tmp/src/main/java/com/liboshuai/demo/JvmDemo.java
@@ -1,11 +1,22 @@
 package com.liboshuai.demo;
 
 public class JvmDemo {
-    public static void main(String[] args) {
-        String s = new String("a") + new String("b");
-        String s2 = s.intern();
 
-        System.out.println(s == "ab"); // true
-        System.out.println(s2 == "ab"); // true
+    private byte[] bigSizeByte = new byte[1024 * 1024 * 5];
+
+    private JvmDemo jvmDemo;
+
+    public void setJvmDemo(JvmDemo jvmDemo) {
+        this.jvmDemo = jvmDemo;
+    }
+
+    public static void main(String[] args) {
+        JvmDemo jvmDemo1 = new JvmDemo();
+        JvmDemo jvmDemo2 = new JvmDemo();
+        jvmDemo1.setJvmDemo(jvmDemo2);
+        jvmDemo2.setJvmDemo(jvmDemo1);
+        jvmDemo1 = null;
+        jvmDemo2 = null;
+//        System.gc();
     }
 }
