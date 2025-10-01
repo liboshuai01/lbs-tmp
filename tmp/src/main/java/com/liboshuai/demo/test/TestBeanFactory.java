f7a5abaff0e0a3285754263b01fce27ea3a52961
Boshuai Li <liboshuai01@gmail.com>
2025-10-02 06:33:54 +0800

    add TestBeanFactory

---
+++ tmp/src/main/java/com/liboshuai/demo/test/TestBeanFactory.java
@@ -0,0 +1,76 @@
+package com.liboshuai.demo.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class TestBeanFactory {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(Config.class).setScope("singleton").getBeanDefinition();
+        beanFactory.registerBeanDefinition("config", beanDefinition);
+
+        // 给 BeanFactory 添加一些常用的后处理器
+        AnnotationConfigUtils.registerAnnotationConfigProcessors(beanFactory);
+
+        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values()
+                .forEach(beanFactoryPostProcessor -> beanFactoryPostProcessor.postProcessBeanFactory(beanFactory));
+
+        // bean 后处理器，针对 bean 的生命周期的各个阶段提供扩展，例如 @Autowired @Resource
+        beanFactory.getBeansOfType(BeanPostProcessor.class).values().stream().sorted(beanFactory.getDependencyComparator()).forEach(beanPostProcessor -> {
+            System.out.println(">>>" + beanPostProcessor);
+            beanFactory.addBeanPostProcessor(beanPostProcessor);
+        });
+
+        for (String name : beanFactory.getBeanDefinitionNames()) {
+            System.out.println("name: " + name);
+        }
+
+        System.out.println(beanFactory.getBean(Bean1.class).getBean2());
+    }
+
+    @Configuration
+    static class Config {
+        @Bean
+        public Bean1 bean1() {
+            return new Bean1();
+        }
+
+        @Bean
+        public Bean2 bean2() {
+            return new Bean2();
+        }
+    }
+
+    static class Bean1 {
+        private static final Logger log = LoggerFactory.getLogger(Bean1.class);
+
+        public Bean1() {
+            log.debug("构造 Bean1()");
+        }
+
+        @Autowired
+        private Bean2 bean2;
+
+        public Bean2 getBean2() {
+            return bean2;
+        }
+    }
+
+    static class Bean2 {
+        private static final Logger log = LoggerFactory.getLogger(Bean2.class);
+
+        public Bean2() {
+            log.debug("构造 Bean2()");
+        }
+    }
+}
