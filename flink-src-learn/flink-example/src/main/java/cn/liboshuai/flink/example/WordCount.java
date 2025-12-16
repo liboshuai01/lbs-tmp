7f7ccfd1f9d0c79819001230de3339d0ec590591
Boshuai Li <liboshuai01@gmail.com>
2025-12-16 13:06:45 +0800

    add WordCount

--- flink-src-learn/flink-example/src/main/java/cn/liboshuai/flink/example/WordCount.java
+++ flink-src-learn/flink-example/src/main/java/cn/liboshuai/flink/example/WordCount.java
@@ -1,8 +1,14 @@
 package cn.liboshuai.flink.example;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class WordCount {
     public static void main(String[] args) throws Exception {
@@ -12,7 +18,14 @@ public class WordCount {
         env.setParallelism(1);
 
         env.socketTextStream("127.0.0.1", 9999)
-                .map(String::toUpperCase)
+                .flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (value, out) -> {
+                    String[] s = value.split(" ");
+                    for (String string : s) {
+                        out.collect(Tuple2.of(string, 1));
+                    }
+                }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy((KeySelector<Tuple2<String, Integer>, String>) value -> value.f0)
+                .sum(1)
                 .print();
 
         env.execute("WordCount");
